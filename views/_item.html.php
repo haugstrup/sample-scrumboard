0a79aede047a9e081d159c01826d34fb48aedcd3
Andreas Haugstrup Pedersen <haugstrup@podio.com>
2011-11-09 12:00:03 +0100

    Use lowercase for google chrome

--- views/_item.html.php
+++ views/_item.html.php
@@ -1,7 +1,7 @@
 <li class="story-item" data-id="<?= $item->item_id ?>" data-url="<?= $_SESSION['space']['url']; ?>item/<?= $item->item_id ?>">
   <h3>
     <?php if ($item->time_left > 0) : ?><div class="timeleft tooltip" title="Original estimate: <?= $item->estimate ?> hrs"><?= $item->time_left ?></div><?php endif; ?>
-    <a target="_BLANK" href="<?= $item->link ?>"><?= $item->title ?></a>
+    <a target="_blank" href="<?= $item->link ?>"><?= $item->title ?></a>
   </h3>
   <div class="story-item-details" <?php if (!$item->responsible) { print 'style="display:none;"'; } ?>>
     <?php if ($item->responsible) : ?>
